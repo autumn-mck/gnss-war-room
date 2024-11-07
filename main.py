95fd076a1e0f3198c93d7a8bc82b170cb5c352bd
40323855 <jmckee54@qub.ac.uk>
2024-11-07 20:11:23 +0000

    import config from json

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import json
 from PyQt6.QtWidgets import QMainWindow, QApplication
 from PyQt6.QtSvgWidgets import QSvgWidget
 from palettes.palette import loadPalette
@@ -8,16 +9,16 @@ class MainWindow(QMainWindow):
 	def __init__(self):
 		super().__init__()
 
-		palette = loadPalette("warGames")
+		with open("config.json", "r") as f:
+			config = json.load(f)
+
+		palette = loadPalette(config["paletteName"])
 
 		self.setWindowTitle("GNSS War Room")
 		self.setGeometry(100, 100, 700, 500)
 		self.setStyleSheet(f"background-color: {palette['background']}; color: {palette['foreground']};")
 
-		focusLat = 54.6
-		focusLong = -6.0
-
-		svgFile = prepareSvg(palette, {"hideRivers": True, "hideLakes": True, "hideAdmin1Borders": True, "hideAdmin0Borders": False, "focusLat": focusLat, "focusLong": focusLong, "scaleFactor": 4})
+		svgFile = prepareSvg(palette, config["mapConfig"])
 		self.map = QSvgWidget(svgFile, parent=self)
 		self.map.setGeometry(0, 0, 700, 500)
 
