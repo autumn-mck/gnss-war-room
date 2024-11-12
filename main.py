5f694ece9c54dfa8214ea2e73931aa8454f274c1
40323855 <jmckee54@qub.ac.uk>
2024-11-12 13:55:16 +0000

    allow multiple windows in config file

--- main.py
+++ main.py
@@ -6,24 +6,28 @@ from palettes.palette import loadPalette
 from mapdata.maps import prepareSvg
 
 class MainWindow(QMainWindow):
-	def __init__(self):
+	def __init__(self, palette, windowConfig):
 		super().__init__()
-
-		with open("config.json", "r") as f:
-			config = json.load(f)
-
-		palette = loadPalette(config["paletteName"])
 
 		self.setWindowTitle("GNSS War Room")
 		self.setGeometry(100, 100, 700, 500)
 		self.setStyleSheet(f"background-color: {palette['background']}; color: {palette['foreground']};")
 
-		svgFile = prepareSvg(palette, config["mapConfig"])
+		svgFile = prepareSvg(palette, windowConfig)
 		self.map = QSvgWidget(svgFile, parent=self)
 		self.map.setGeometry(0, 0, 700, 500)
 
 		self.show()
 
 app = QApplication(sys.argv)
-w = MainWindow()
+
+with open("config.json", "r") as f:
+	appConfig = json.load(f)
+
+palette = loadPalette(appConfig["paletteName"])
+
+windows = []
+for windowConfig in appConfig["windows"]:
+	windows.append(MainWindow(palette, windowConfig))
+
 app.exec()
