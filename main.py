eb1592fb0aff6b9136520a04e55d1e7bc4999855
40323855 <jmckee54@qub.ac.uk>
2024-11-16 14:31:39 +0000

    move map around with keybinds

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 from dataclass_wizard import JSONWizard
 from PyQt6.QtWidgets import QMainWindow, QApplication
 from PyQt6.QtSvgWidgets import QSvgWidget
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QResizeEvent, QKeyEvent
 from palettes.palette import loadPalette, Palette
 from mapdata.maps import readBaseSvg, prepareSvg, focusOnPoint, saveToTempFile, MapConfig
 
@@ -40,7 +42,7 @@ class MapWindow(QMainWindow):
 		else:
 			self.show()
 
-	def resizeEvent(self, event):
+	def resizeEvent(self, event: QResizeEvent):
 		"""Resize map when window is resized"""
 		newX = event.size().width()
 		newY = event.size().height()
@@ -54,6 +56,34 @@ class MapWindow(QMainWindow):
 		self.map.load(self.svgFile)
 		self.map.setGeometry(0, 0, newX, newY)
 
+	def moveMapBy(self, lat: float, long: float):
+		"""Move the map by a given amount"""
+		self.windowConfig.focusLat += lat
+		self.windowConfig.focusLong += long
+
+		with open(self.svgFile, "r", encoding="utf8") as f:
+			mapSvg = f.read()
+
+		mapSvg = focusOnPoint(mapSvg, self.windowConfig, self.map.width(), self.map.height())
+		self.svgFile = saveToTempFile(mapSvg)
+
+		self.map.load(self.svgFile)
+		self.map.setGeometry(0, 0, self.map.width(), self.map.height())
+
+	# key bindings
+	def keyPressEvent(self, event: QKeyEvent):
+		toMove = 2 / self.windowConfig.scaleFactor
+		if event.modifiers() == Qt.KeyboardModifier.ShiftModifier:
+			toMove *= 5
+
+		if event.key() == Qt.Key.Key_W:
+			self.moveMapBy(toMove, 0)
+		if event.key() == Qt.Key.Key_S:
+			self.moveMapBy(-toMove, 0)
+		if event.key() == Qt.Key.Key_A:
+			self.moveMapBy(0, -toMove)
+		if event.key() == Qt.Key.Key_D:
+			self.moveMapBy(0, toMove)
 
 def main():
 	"""Main function"""
