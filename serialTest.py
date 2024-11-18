2f08a071df7ce49f34dd6a6052ac7495158893cf
40323855 <jmckee54@qub.ac.uk>
2024-11-18 15:17:14 +0000

    fix reading serial on windows

--- serialTest.py
+++ serialTest.py
@@ -1,9 +1,13 @@
+import os
 from serial import Serial
 from pynmeagps import NMEAReader
 
 LINES_TO_LOG = 500
+SERIAL_LOCATION = '/dev/ttyUSB0' # default to linux
+if os.name == 'nt':
+	SERIAL_LOCATION = 'COM4' # on windows, COM4 is default for my receiver? although the other one defaults to COM3
 
-with Serial('/dev/ttyUSB0', 38400, timeout=3) as stream:
+with Serial(SERIAL_LOCATION, 38400, timeout=3) as stream:
 	nmr = NMEAReader(stream)
 	with open('test.nmea', 'w', encoding='utf-8') as logFile:
 		for _ in range(LINES_TO_LOG):
