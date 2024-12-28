0e4c425663b99d5cc24db8cd0c6fcd0a78585be3
40323855 <jmckee54@qub.ac.uk>
2024-12-28 20:07:17 +0000

    download font files on startup if not already there

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import os
 import sys
+import urllib.request
 
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtWidgets import QMainWindow
@@ -14,6 +16,8 @@ from miscStatsWindow import MiscStatsWindow
 
 def main():
 	"""Main function"""
+	fetchHp1345FilesIfNeeded()
+
 	app = QApplication(sys.argv)
 
 	appConfig = loadConfig()
@@ -44,6 +48,21 @@ def handleMultiScreen(screens: list, window: QMainWindow, index: int):
 	screen = screens[index]
 	qr = screen.geometry()
 	window.move(qr.left(), qr.top())
+
+def fetchHp1345FilesIfNeeded():
+	"""Download the HP1345A font files if they don't exist"""
+	if os.path.isfile("./font/01347-80012.bin"):
+		return
+
+	print("Downloading HP1345 font files...")
+	charRomUrl = "https://phk.freebsd.dk/_downloads/a89c073235ca9c2b13d657173d32bf78/01347-80012.bin"
+	charIndexRomUrl = "https://phk.freebsd.dk/_downloads/2355976608a6359335e30a88e181f1fc/1816-1500.bin"
+	firmwareRomUrl = "https://phk.freebsd.dk/_downloads/13f169d8d8dff52497dca435d649f3d0/01347-80010.bin"
+
+	urllib.request.urlretrieve(charRomUrl, "./font/01347-80012.bin")
+	urllib.request.urlretrieve(charIndexRomUrl, "./font/1816-1500.bin")
+	urllib.request.urlretrieve(firmwareRomUrl, "./font/01347-80010.bin")
+	print("Done downloading HP1345 font files")
 
 if __name__ == "__main__":
 	main()
