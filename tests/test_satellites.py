7cac598b0df2b68824cfa97c81178ac69e23a225
40323855 <jmckee54@qub.ac.uk>
2025-02-06 12:35:00 +0000

    fix getSatelliteLatLong tests

--- tests/test_satellites.py
+++ tests/test_satellites.py
@@ -1,10 +1,39 @@
 # pylint: skip-file
 from pytest import approx
-from gnss.satellite import SatelliteInView, getSatelliteLatLong
+from gnss.satellite import getSatelliteLatLong
 
 
-def test_getSatelliteLatLong():
-	"""Test that the lat/long is calculated correctly"""
-	(lat, long) = getSatelliteLatLong(SatelliteInView(azimuth=0, elevation=0))
-	assert lat == approx(72.3421)
+def test_satelliteDirectlyAbove():
+	(lat, long) = getSatelliteLatLong(0, 90, "GA", 0, 0)
+	assert lat == 0
 	assert long == 0
+
+
+def test_angledElevation():
+	(lat, long) = getSatelliteLatLong(0, 45, "GA", 0, 0)
+	assert lat == approx(33.8157)
+	assert long == 0
+
+
+def test_rotated90():
+	(lat, long) = getSatelliteLatLong(90, 45, "GA", 0, 0)
+	assert lat == approx(0)
+	assert long == approx(33.8157)
+
+
+def test_mixedRotation():
+	(lat, long) = getSatelliteLatLong(30, 45, "GA", 0, 0)
+	assert lat == approx(28.8137)
+	assert long == approx(18.5167)
+
+
+def test_alternateNetwork():
+	(lat, long) = getSatelliteLatLong(30, 45, "GP", 0, 0)
+	assert lat == approx(29.94983)
+	assert long == approx(19.4303)
+
+
+def test_alternateMeasurementPoint():
+	(lat, long) = getSatelliteLatLong(30, 45, "GA", 45, 10)
+	assert lat == approx(68.1693)
+	assert long == approx(58.442)
