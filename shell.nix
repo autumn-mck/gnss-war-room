4274237be960efbd73bc1f767642c523368c1098
40323855 <jmckee54@qub.ac.uk>
2025-02-14 23:19:35 +0000

    update nixpkgs version

--- shell.nix
+++ shell.nix
@@ -2,8 +2,8 @@
 # Note: This only covers running the web API, not the PyQt GUI.
 let
   # We pin to a specific nixpkgs commit for reproducibility.
-  # Last updated: 2025-01-03. Check for new commits at https://status.nixos.org.
-  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/d6973081434f88088e5321f83ebafe9a1167c367.tar.gz") { };
+  # Last updated: 2025-02-14. Check for new commits at https://status.nixos.org.
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/1128e89fd5e11bb25aedbfc287733c6502202ea9.tar.gz") { };
 in
 pkgs.mkShell {
   packages = [
