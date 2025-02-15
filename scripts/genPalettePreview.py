31e561b7dd2389650737e89d7665bb5340b49bfd
40323855 <jmckee54@qub.ac.uk>
2025-02-15 01:05:01 +0000

    finished pylinting

--- scripts/genPalettePreview.py
+++ scripts/genPalettePreview.py
@@ -1,3 +1,4 @@
+from typing import Any
 from palettes.palette import loadPalette
 
 
@@ -10,39 +11,20 @@ def main():
 
 	svg = ""
 
-	coloursToIgnore = ["#ff0000"]
+	coloursToIgnore = ["#ff0000", "#FF0000"]
 	attributesToIgnore = ["water"]
 
 	count = 0
 	coloursAlreadyUsed = []
-	for attr, value in palette.__dict__.items():
-		if attr in attributesToIgnore:
-			continue
-
-		if isinstance(value, str):
-			value = value.lower()
-			if value in coloursAlreadyUsed:
-				continue
-			if value in coloursToIgnore:
-				continue
-			coloursAlreadyUsed.append(value)
-			svg += f"""<rect
-			x="{(squareSize + gapSize) * count + gapSize}" y="{gapSize}"
-			width="{squareSize}" height="{squareSize}"
-			fill="{value}" />\n"""
-			count += 1
-		elif isinstance(value, dict):
-			for key, value in value.items():
-				if key in coloursToIgnore:
-					continue
-				if key in coloursAlreadyUsed:
-					continue
-				coloursAlreadyUsed.append(key)
-				svg += f"""<rect
-				x="{(squareSize + gapSize) * count + gapSize}" y="{gapSize}"
-				width="{squareSize}" height="{squareSize}"
-				fill="{value}" />\n"""
-				count += 1
+	(svg, count) = paletteForColoursInDict(
+		palette.__dict__,
+		coloursToIgnore,
+		attributesToIgnore,
+		count,
+		gapSize,
+		squareSize,
+		coloursAlreadyUsed,
+	)
 
 	width = gapSize + (squareSize + gapSize) * count
 	height = gapSize * 2 + squareSize
@@ -58,6 +40,47 @@ def main():
 
 	with open("palettePreview.svg", "w", encoding="utf8") as f:
 		f.write(svg)
+
+
+def paletteForColoursInDict(
+	colours: dict[str, Any],
+	coloursToIgnore: list[str],
+	attributesToIgnore: list[str],
+	count: int,
+	gapSize: int,
+	squareSize: int,
+	coloursAlreadyUsed: list[str],
+) -> tuple[str, int]:
+	"""Generate rectangles for each colour in the given dict"""
+	svg = ""
+	for key, value in colours.items():
+		if key in attributesToIgnore:
+			continue
+		if value in coloursAlreadyUsed:
+			continue
+		if value in coloursToIgnore:
+			continue
+		if isinstance(value, str):
+			# value = value.lower()
+			coloursAlreadyUsed.append(value)
+			svg += f"""<rect
+			x="{(squareSize + gapSize) * count + gapSize}" y="{gapSize}"
+			width="{squareSize}" height="{squareSize}"
+			fill="{value}" />\n"""
+			count += 1
+		elif isinstance(value, dict):
+			(svgExtra, count) = paletteForColoursInDict(
+				value,
+				coloursToIgnore,
+				attributesToIgnore,
+				count,
+				gapSize,
+				squareSize,
+				coloursAlreadyUsed,
+			)
+			svg += svgExtra
+
+	return (svg, count)
 
 
 if __name__ == "__main__":
