9928bb628a174e915f90ac00e411ca33c9d016ae
40323855 <jmckee54@qub.ac.uk>
2025-02-18 11:35:50 +0000

    remove unused import

--- views/rawMessages/window.py
+++ views/rawMessages/window.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import QMainWindow
 from PyQt6.QtSvgWidgets import QSvgWidget
-from PyQt6.QtCore import pyqtSignal, QByteArray
+from PyQt6.QtCore import QByteArray
 from PyQt6.QtGui import QResizeEvent
 
 from font.hp1345Font import Font
