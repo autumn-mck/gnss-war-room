c587c37af98b2730bc55a49b1645f222c4655398
40323855 <jmckee54@qub.ac.uk>
2025-02-19 10:06:26 +0000

    add bun to nix-shell and format

--- shell.nix
+++ shell.nix
@@ -3,19 +3,25 @@
 let
   # We pin to a specific nixpkgs commit for reproducibility.
   # Last updated: 2025-02-14. Check for new commits at https://status.nixos.org.
-  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/1128e89fd5e11bb25aedbfc287733c6502202ea9.tar.gz") { };
+  pkgs =
+    import
+      (fetchTarball "https://github.com/NixOS/nixpkgs/archive/1128e89fd5e11bb25aedbfc287733c6502202ea9.tar.gz")
+      { };
 in
 pkgs.mkShell {
-  packages = [
-    (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
-      # select Python packages here
-			pynmeagps
-			paho-mqtt_2
-      pyjson5
-			dataclass-wizard
-			flask
-			gunicorn
-      h3
-    ]))
+  packages = with pkgs; [
+    bun
+
+    (python3.withPackages (
+      python-pkgs: with python-pkgs; [
+        pynmeagps
+        paho-mqtt_2
+        pyjson5
+        dataclass-wizard
+        flask
+        gunicorn
+        h3
+      ]
+    ))
   ];
 }
