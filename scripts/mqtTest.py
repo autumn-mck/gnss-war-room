e8c2c2191f0018bb49fd42db903d7eae0ceb0499
40323855 <jmckee54@qub.ac.uk>
2025-02-23 19:28:50 +0000

    improve mqtt graphing, use actual messages, and qos=2

--- scripts/mqtTest.py
+++ scripts/mqtTest.py
@@ -18,15 +18,40 @@ def main():
 	publisher = createMqttPublishers([config.mqtt])[0]
 	subscriber = createMqttClient(config.mqtt)
 
-	times: list[float] = []
-	for i in range(100_000):
-		timeMs = sendReceiveMessage(publisher, subscriber, f"who up webbing they fish {i}")
-		print(timeMs)
-		if timeMs < 80:
-			times.append(timeMs)
+	with open("120k.nmea", "r", encoding="utf-8") as f:
+		lines = f.readlines()
 
-	plt.hist(times, bins=100)
+	times: list[float] = []
+	numMessages = 10_000
+	for i in range(numMessages):
+		timeMs = sendReceiveMessage(publisher, subscriber, lines[i])
+		print(f"{(i / numMessages * 100):.2f}% done")
+		times.append(timeMs)
+
+	print(f"Median: {calcPercentile(times, 50):.2f}ms")
+	print(f"90th percentile: {calcPercentile(times, 90):.2f}ms")
+	print(f"99th percentile: {calcPercentile(times, 99):.2f}ms")
+	print(f"99.9th percentile: {calcPercentile(times, 99.9):.2f}ms")
+	upperBound = calcPercentile(times, 99.95)
+
+	fig = plt.figure()
+	axs = fig.subplots(1, 1)
+	plt.hist(times, bins=500, cumulative=True, range=(0, upperBound), density=True)
+
+	axs.set_title("RTT for MQTT messages (Different network)")
+	axs.set_xlabel("Time (ms)")
+	axs.set_ylabel("Probability message arrived (cumulative)")
+
+	axs.set_xlim(0, upperBound)
+	axs.set_ymargin(0.03)
+
 	plt.show()
+
+
+def calcPercentile(times: list[float], percentile: float) -> float:
+	sortedTimes = sorted(times)
+	index = int(len(sortedTimes) * (percentile / 100))
+	return sortedTimes[index]
 
 
 def createMqttClient(config: MqttConfig) -> MqttClient:
@@ -36,7 +61,7 @@ def createMqttClient(config: MqttConfig) -> MqttClient:
 	mqttClient.on_connect = print
 	mqttClient.on_message = print
 	mqttClient.connect(config.host, config.port)
-	mqttClient.subscribe("gnss/rawMessages", qos=0)
+	mqttClient.subscribe("gnss/rawMessages", qos=2)
 	mqttClient.loop_start()
 	return mqttClient
 
@@ -52,7 +77,7 @@ def sendReceiveMessage(publisher: MqttClient, subscriber: MqttClient, message: s
 
 	subscriber.on_message = onMessage
 	sentTime = time.time()
-	publisher.publish("gnss/rawMessages", message, qos=0)
+	publisher.publish("gnss/rawMessages", message, qos=2)
 
 	while receivedTime is None:
 		time.sleep(0.001)
