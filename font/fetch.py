51217075d60ed185185478004e9ff697375c14f6
40323855 <jmckee54@qub.ac.uk>
2025-04-19 22:22:25 +0000

    make font rom fetching more robust by checking each file individually

--- font/fetch.py
+++ font/fetch.py
@@ -4,21 +4,15 @@ import urllib.request
 
 def fetchFontRomsIfNeeded():
 	"""Download the HP1345A font files if they don't exist"""
-	if os.path.isfile("./font/01347-80012.bin"):
-		return
 
-	print("Downloading HP1345 font files...")
-	charRomUrl = (
-		"https://phk.freebsd.dk/_downloads/a89c073235ca9c2b13d657173d32bf78/01347-80012.bin"
-	)
-	charIndexRomUrl = (
-		"https://phk.freebsd.dk/_downloads/2355976608a6359335e30a88e181f1fc/1816-1500.bin"
-	)
-	firmwareRomUrl = (
-		"https://phk.freebsd.dk/_downloads/13f169d8d8dff52497dca435d649f3d0/01347-80010.bin"
-	)
+	baseUrl = "https://phk.freebsd.dk/_downloads"
+	roms = {
+		"01347-80012.bin": f"{baseUrl}/a89c073235ca9c2b13d657173d32bf78/01347-80012.bin",
+		"1816-1500.bin": f"{baseUrl}/2355976608a6359335e30a88e181f1fc/1816-1500.bin",
+		"01347-80010.bin": f"{baseUrl}/13f169d8d8dff52497dca435d649f3d0/01347-80010.bin",
+	}
 
-	urllib.request.urlretrieve(charRomUrl, "./font/01347-80012.bin")
-	urllib.request.urlretrieve(charIndexRomUrl, "./font/1816-1500.bin")
-	urllib.request.urlretrieve(firmwareRomUrl, "./font/01347-80010.bin")
-	print("Done downloading HP1345 font files")
+	for rom, url in roms.items():
+		if not os.path.isfile(f"./font/{rom}"):
+			print(f"ROM file missing, downloading {rom}...")
+			urllib.request.urlretrieve(url, f"./font/{rom}")
